c754c5bf4f2fe65cc9a06a02b04c9d202ad7a253
Andres H <andres.holguin.mt@corro.usa.edu.co>
2021-10-13 21:14:15 -0500

    prueba

---
+++ src/main/java/minticg25/g25/G25Application.java
@@ -0,0 +1,13 @@
+package minticg25.g25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G25Application.class, args);
+	}
+
+}
---
+++ src/test/java/minticg25/g25/G25ApplicationTests.java
@@ -0,0 +1,13 @@
+package minticg25.g25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
